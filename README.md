3bbc22513c17b90e13f6f58f30451e4945b6bffe
Developer199239 <developer199239@gmail.com>
2024-08-19 00:46:39 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# basic_widget_2
+## Screenshots
+
+<img src="images/screenshot.png" width="300" height="500" alt="Screenshot 1">
+
---
+++ android/app/src/main/kotlin/com/example/basic_widget_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.basic_widget_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
